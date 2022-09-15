7c4f8d5d46ac5165852c0ef665858a56f41e1bd3
DESKTOP-8BT5F42\Work <ihsanmohamad13@gmail.com>
2022-09-15 16:08:09 +0800

    hero section (WIP)

--- src/lib/components/HeroSection.svelte
+++ src/lib/components/HeroSection.svelte
@@ -1,12 +1,14 @@
 <script lang="ts">
-	import { Container, createStyles } from "@svelteuidev/core";
+	import { Container, Group, Paper, Grid, createStyles } from "@svelteuidev/core";
 
     const useStyles = createStyles(() => ({
         root: {
+            mt: '$10',
+            borderRadius: '$lg',
             zIndex: '$2',
             position: 'relative',
-            px: '$32',
-            height: '500px',
+            px: '$0',
+            height: '600px',
             overflow: 'hidden',
             background: 'linear-gradient(180deg, #C90027 -2.78%, #B90C2E 75.97%, #97001E 97.06%)',
             '&::before': {
@@ -14,26 +16,72 @@
                 position: 'absolute',
                 background: 'white',
                 transform: 'rotate(-5deg)',
-                top: '-50%',
+                top: '-43%',
                 left: '-30%',
                 width: '5000px',
-                height: '300px',
+                height: '400px',
                 zIndex: -1
+            },
+            '@lg': {
+                px: '$32'
             }
         },
         inner: {
             zIndex: '$5',
 			display: 'flex',
+            flexDirection: 'column',
 			justifyContent: 'space-between',
-			alignItems: 'center'
+            mt: '$10',
+            '@md': {
+                flexDirection: 'row',
+                mt: '$0'
+            }
 		},
+        heroTitle: {
+            fontSize: '40px'
+        },
+        heroCaption: {
+            fontFamily: '$standard',
+            fontStyle: 'normal',
+            fontWeight: 400,
+            fontSize: '20px',
+            lineHeight: '150%',
+            color: '#6A6D9E'
+        },
+        heroImage: {
+            display: 'none',
+            '@md': {
+                display: 'block',
+            }
+        },
+        bookingBox: {
+            zIndex: 10,
+            my: '$12',
+            padding: '$12',
+            borderRadius: '20px',
+            boxShadow: '0px 25px 50px rgba(0, 0, 0, 0.25)',
+            border: '$black'
+        }
 	}));
 
     $: ({classes} = useStyles())
 </script>
 <div class={classes.root}>
     <Container fluid class={classes.inner}>
-        <h1>Welcome to SvelteKit</h1>
-        <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+        <Group direction="column" align="flex-start">
+            <h1 class={classes.heroTitle}>What will you be driving today?</h1>
+            <p class={classes.heroCaption}>Avis provides customers professional mobility services and solutions specializing in vehicle rental, leasing and fleet management.</p>
+        </Group>
+        <img class={classes.heroImage} src="car.png" alt="nice looking car" />
+    </Container>
+    <Container>
+        <Paper class={classes.bookingBox} withBorder>
+            <Grid>
+                <Grid.Col md={6} lg={3}>1</Grid.Col>
+                <Grid.Col md={6} lg={3}>2</Grid.Col>
+                <Grid.Col md={6} lg={3}>3</Grid.Col>
+                <Grid.Col md={6} lg={3}>4</Grid.Col>
+            </Grid>
+          </Paper>
     </Container>
 </div>
